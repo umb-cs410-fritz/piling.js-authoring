cad434acf1a1f410994b429588b23b1ac8f73265
Emily Gagne <ceg9498@gmail.com>
2022-05-11 10:57:03 -0400

    Revert "Add files via upload"
    This reverts commit bdf9e1e0dbdb111709dcf2c199cece0b598f4a1b.

--- src/repl/Repl.svelte
+++ src/repl/Repl.svelte
@@ -1,5 +1,10 @@
 <script>
-  import { setContext, getContext, createEventDispatcher, onDestroy } from 'svelte';
+  import {
+    setContext,
+    getContext,
+    createEventDispatcher,
+    onDestroy,
+  } from 'svelte';
   import { writable } from 'svelte/store';
   import SplitPane from './SplitPane.svelte';
   import ComponentSelector from './Input/ComponentSelector.svelte';
@@ -10,18 +15,22 @@
   import PaneWithPanel from './Output/PaneWithPanel.svelte';
   import CodeMirror from './CodeMirror.svelte';
   import { spring } from 'svelte/motion';
+  import Styles from './Styles.svelte';
+  import Sidebar from '../Sidebar.svelte';
 
   import {
     components,
     debug,
     selectedComponent as selected,
     autoRun,
-    prevPilingState
+    prevPilingState,
   } from '../stores.js';
 
   import {
     DEFAULT_DATA_NAME,
+    DEFAULT_STYLES_NAME,
     DATA_JSON_INDEX,
+    STYLES_JS_INDEX,
     INTERMEDIATE_APP_MAP,
   } from '../constants.js';
 
@@ -36,6 +45,8 @@
   export let injectedJS = '';
   export let injectedCSS = '';
   export let openLoadDataModal;
+
+  let styles_var;
 
   const topEditorHistoryMap = new Map();
   const historyMap = new Map();
@@ -119,40 +130,47 @@
 
   let current_token;
   export async function rebundle() {
-
     const token = (current_token = {});
     // bundle components along with previous piling state (unless in debug mode)
     const result = await bundler.bundle([
       ...$components,
       {
-      type: 'js',
-      name: 'piling-state',
-      source: `const prevPilingState = ${$debug ? null : $prevPilingState};export default prevPilingState;`
-      }
+        type: 'js',
+        name: 'piling-state',
+        source: `const prevPilingState = ${
+          $debug ? null : $prevPilingState
+        };export default prevPilingState;`,
+      },
     ]);
     if (result && token === current_token) bundle.set(result);
-    // update selected component    
+    // update selected component
     if (!$selected) {
       // select first component and reset editor
       handle_select($components[0]);
     } else {
       // keep the same component if still there, otherwise select first component and reset editor
-      const newComponent = $components.find(el => {
-        return el.name === $selected.name && el.type === $selected.type
+      const newComponent = $components.find((el) => {
+        return el.name === $selected.name && el.type === $selected.type;
       });
       if ($selected !== newComponent) {
         handle_select(newComponent || $components[0]);
-      };
+      }
     }
-    console.log("access rebundle: OK!");
   }
 
   let view;
   $: view === 'intermediate' ? bundleIntermediate() : rebundle();
 
   async function bundleIntermediate() {
-    console.log("access bundleIntermediate: OK!");
-  }  
+    const token = (current_token = {});
+    const intermediateApp =
+      INTERMEDIATE_APP_MAP[`${$selected.name}.${$selected.type}`] ||
+      $components[0];
+    const result = await bundler.bundle(
+      $components.slice(1).concat([intermediateApp])
+    );
+    if (result && token === current_token) bundle.set(result);
+  }
 
   // TODO this is a horrible kludge, written in a panic. fix it
   let fulfil_module_editor_ready;
@@ -237,6 +255,37 @@
       });
     },
 
+    // my code starts
+    handle_styles_change: (event) => {
+      console.log('this is event obj' + event);
+      console.log(event);
+      var styles_var = `const styles = {
+            columns: ${event.target.value},
+            };
+          export default styles;`;
+      components.update((components) => {
+        // TODO this is a bit hacky — we're relying on mutability
+        // so that updating components works... might be better
+        // if a) components had unique IDs, b) we tracked selected
+        // *index* rather than component, and c) `selected` was
+        // derived from `components` and `index`
+        components[STYLES_JS_INDEX].source = styles_var;
+        return components;
+      });
+
+      components.update((c) => c);
+
+      // recompile selected component
+      output.update($components[STYLES_JS_INDEX], $compile_options);
+
+      $autoRun && rebundle();
+
+      dispatch('change', {
+        components: $components,
+      });
+    },
+    //my code ends
+
     register_module_editor(editor) {
       module_editor = editor;
       fulfil_module_editor_ready();
@@ -253,6 +302,7 @@
   });
 
   const { handle_data_change } = getContext('REPL');
+  const { handle_styles_change } = getContext('REPL');
 
   const handle_select_historyMap = (historyMap, component) => {
     historyMap.set(get_component_name($selected), module_editor.getHistory());
@@ -392,7 +442,7 @@
       <ComponentSelector {handle_select} handle_data_select={initTop} />
       <PaneWithPanel
         bind:pos
-        panel={$selected.name === DEFAULT_DATA_NAME ? 'Data Transformer' : 'Custom Code'}>
+        panel={$selected.name === DEFAULT_DATA_NAME ? 'Data Transformer' : 'Custom Codes'}>
         <div slot="main">
           <div class="panel-header" on:click={toggleTop}>
             <h3>
@@ -410,7 +460,11 @@
                 on:change={handle_data_change}
                 on:editorReady={fulfillDataEditorReady} />
             </div>
-          {:else}buttons here{/if}
+          {:else if $selected.name === DEFAULT_STYLES_NAME}
+            <Styles on:change={handle_styles_change} />
+          {:else if $selected.name == 'sidebar'}
+            <Sidebar/>
+          {/if}
         </div>
 
         <div slot="panel-body" style="display: flex; height: 100%;">
