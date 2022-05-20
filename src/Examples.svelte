4479aec66196a5fce8818dbfc2d440352e2b7838
Tung Duong <Tung.Duong002@umb.edu>
2022-05-19 22:49:34 -0400

    Intermediate View Update

--- src/Examples.svelte
+++ src/Examples.svelte
@@ -3,6 +3,7 @@
   const booksUserAndGistId = 'flekschas/73dd0de929033ddadb0313fa46257835';
   const ridgePlotUserAndGistId = 'flekschas/81e7fb4f9cf5946f12639c383af97dc6';
   const matrixUserAndGistId = 'flekschas/0bee19015958e5677a1fb476712b3803';
+  const cuteDogsAndGistId = 'TungDuong2/f72775662cc23a4a2ce7cfae56025f36';
 
   export let refreshHandler;
 </script>
@@ -31,6 +32,9 @@
     <ul>
       <li>
         <Example userAndGistId={booksUserAndGistId} refreshHandler={refreshHandler} />
+      </li>
+      <li>
+        <Example userAndGistId={cuteDogsAndGistId} refreshHandler={refreshHandler} />
       </li>
     </ul>
   </li>
